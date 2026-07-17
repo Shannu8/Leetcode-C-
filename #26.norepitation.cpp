5e2bf4e66874452d5ea49a0de3bd1462ac91894f
Muhammed Shanwaz <shannuknr8@gmail.com>
2026-07-17 22:04:43 +0530

    Create #26.norepitation.cpp

---
+++ #26.norepitation.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            int j = i + 1;
+            while (j < n) {
+                if (nums[i] == nums[j]) {
+                    for (int k = j; k < n - 1; k++) {
+                        nums[k] = nums[k + 1];
+                    }
+                    n--;
+                }
+                else {
+                    j++;
+                }
+            }
+        }
+        return n;
+    }
+};
