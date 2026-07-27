827e116543efc07987190819745316de52337cb4
Muhammed Shanwaz <shannuknr8@gmail.com>
2026-07-27 22:11:59 +0530

    Implement palindrome number check in C++

---
+++ #9.palindrome-num.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        bool flag = 0;
+        int a = x,num;
+        long long int rev = 0;
+        if(a<0){
+            flag = 0;
+        }
+        else{
+            while(a!=0){
+                num = a % 10;
+                rev = rev*10 + num;
+                a = a/10;
+            }
+        }
+        if(x == rev){
+            flag = 1;
+        }
+        return flag;
+    }
+};
