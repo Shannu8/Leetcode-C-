321ac582fb869510228632dc644561daa3bf08c1
Muhammed Shanwaz <shannuknr8@gmail.com>
2026-08-04 15:49:35 +0530

    Add function to find missing elements in array
    Implement a solution to find missing elements in a sorted array.

---
+++ #3731.Find_Missing_Elements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+       sort(nums.begin(), nums.end());
+        vector<int> ans;
+        int ele = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            while(nums[i] != ele + 1){
+                ans.push_back(++ele);
+            }
+            ele = nums[i];
+        }
+        return ans;
+    }
+};
