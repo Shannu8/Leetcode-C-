8c9827d7f524f68cdcb65841727ddbf51f6b6725
Muhammed Shanwaz <shannuknr8@gmail.com>
2026-08-22 21:57:52 +0530

    Create #3622.Check_Divisibility_by_Digit_Sum_and_Product.cpp

---
+++ #3622.Check_Divisibility_by_Digit_Sum_and_Product.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int x = n, a;
+        int sum = 0, mul = 1;
+
+
+        while(x!=0){
+            a = x % 10;
+            sum = sum+a;
+            mul = mul * a;
+
+
+            x = x /10;  
+        }
+
+        if(n%(sum+mul) == 0){
+            return 1;
+        }
+        else return 0;
+    }
+};
